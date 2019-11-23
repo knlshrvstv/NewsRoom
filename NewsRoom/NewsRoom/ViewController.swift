51cb6fb606642628503a58e9e167059aedc4e68f
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-23 14:33:49 -0800

    Setup Xcode iOS project
    Sets up a vanilla Xcode iOS project

---
+++ NewsRoom/NewsRoom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/23/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
