5c622d991969f39c9a6cef9962634b23399cc824
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-24 15:16:16 -0800

    [Util] Adds custom generic subscript support on UserDefaults
    The issue:
    UserDefaults access verbose and can be shortened. Also, it’s not type safe.
    
    This CR:
    Adds custom subscript  and key that are fixed on a type T

---
+++ NewsRoom/NewsRoom/UserDefaults+Util.swift
@@ -0,0 +1,37 @@
+//
+//  UserDefaults+Util.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/24/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+    struct Key<Value> {
+        var name: String
+    }
+    
+    subscript<T>(key: Key<T>) -> T? {
+        get {
+            return value(forKey: key.name) as? T
+        }
+        
+        set {
+            set(newValue, forKey: key.name)
+        }
+    }
+    
+    subscript<T>(
+        key: Key<T>,
+        default defaultProvider: @autoclosure () -> T) -> T {
+        get {
+            return value(forKey: key.name) as? T
+                ?? defaultProvider()
+        }
+        set {
+            setValue(newValue, forKey: key.name)
+        }
+    }
+}
