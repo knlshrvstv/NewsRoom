8f92f24d24c8922eaaae5890de944cf9b928939b
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-24 15:09:35 -0800

    [ViewModel] Adds a Translator
    The issue:
    We want to be able to translate the new articles based on certain rules. This class currently provides a placeholder for the algorithm to translate text.
    
    This CR:
    Adds a Translator

---
+++ NewsRoom/NewsRoom/Translator.swift
@@ -0,0 +1,16 @@
+//
+//  Translator.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/24/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import Foundation
+
+class Translator {
+    func translate(articleGroup: ArticleGroup, to language: Language) -> ArticleGroup? {
+        // TODO: Implement translation algorithm
+        return ArticleGroup(articles: [Article(title: "Marian Title Test", images: [Article.Image(topImage: true, url: "URL", width: 100, height: 100)], body: "Martian Body Test")], language: .martian)
+    }
+}
