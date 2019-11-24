611b92d898ec75a9e10386c0fb984659042bffb9
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-24 15:07:21 -0800

    [ViewModel] Adds a thread-safe NewsStore that acts as in-memory cache
    The issue:
    We want to be able to store the news article. This class currently doesn’t provide full-fledged caching functionality but in the future this will be extended to provide in-memory and on-disc caching. One reason we can’t do this right now is because the API doesn’t support paging. Also, it doesn’t provide a way to uniquely identify a response and a TTL value of the response. When these are available, we can use these to implement a cache based on the TTL, page, and the unique identifier.
    
    This CR:
    Adds a thread-safe news store

---
+++ NewsRoom/NewsRoom/NewsStore.swift
@@ -0,0 +1,31 @@
+//
+//  NewsStore.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/23/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import Foundation
+
+
+class NewsStore {
+    private var latestLanguageNewsMap: [String: ArticleGroup]
+    private let queue = DispatchQueue(label: "NewsStoreQueue", attributes: .concurrent)
+    
+    init(latestLanguageNewsMap: [String: ArticleGroup] = [:]) {
+        self.latestLanguageNewsMap = latestLanguageNewsMap
+    }
+    
+    func setArticleGroup(language: Language, articleGroup: ArticleGroup) {
+        queue.async(flags: .barrier) { [unowned self] in
+            self.latestLanguageNewsMap[language.rawValue] = articleGroup
+        }
+    }
+    
+    func getArticleGroup(language: Language) -> ArticleGroup? {
+        queue.sync {
+            return latestLanguageNewsMap[language.rawValue]
+        }
+    }
+}
