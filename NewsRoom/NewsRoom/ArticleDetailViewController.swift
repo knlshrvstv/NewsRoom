05fa0583f1cb3d2540189ef12b142de8aed27e02
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-24 15:20:36 -0800

    [View] Adds ArticlesViewController to show list of articles
    This CR:
    Adds the ArticlesViewController to show the list of articles. Additionally, it also houses a switch to change the language preference.

---
+++ NewsRoom/NewsRoom/ArticleDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ArticleDetailViewController.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/24/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
