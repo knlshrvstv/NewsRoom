066478a85e01c8e58a83440ce2c79e5322fc6a74
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-24 14:52:43 -0800

    [View] Add a custom language switch view
    The issue:
    We want to be able to switch between languages. The switch will enable us in that when used on the ArticlesViewController.
    
    This CR:
    Adds a custom ArticlesViewController
    
    Testing done:
    Manaul testing to ensure that it changes state and the label text gets updated accordingly

---
+++ NewsRoom/NewsRoom/LanguageSwitchView.swift
@@ -0,0 +1,66 @@
+//
+//  LanguageSwitchView.swift
+//  NewsRoom
+//
+//  Created by Kunal Shrivastava on 11/24/19.
+//  Copyright © 2019 Kunal Shrivastava. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct LanguageSwitchViewModel {
+    var language: Language
+    let languageChangeAction: (Language) -> Void
+    
+}
+
+class LanguageSwitchView: UIView {
+    private let languageSwitch = UISwitch(frame: .zero)
+    private let languageLabel = UILabel()
+    private var viewModel: LanguageSwitchViewModel
+    
+    init(viewModel: LanguageSwitchViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(frame: .zero)
+        
+        backgroundColor = .green
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        languageLabel.text = viewModel.language.rawValue
+        languageSwitch.isOn = viewModel.language == .martian
+        
+        languageSwitch.addTarget(self, action: #selector(switchChangedState), for: UIControl.Event.valueChanged)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        languageSwitch.translatesAutoresizingMaskIntoConstraints = false
+        languageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(languageSwitch)
+        addSubview(languageLabel)
+        
+        let guide = safeAreaLayoutGuide
+        
+        languageLabel.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        languageLabel.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+        languageLabel.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+        languageLabel.leadingAnchor.constraint(equalTo: languageSwitch.trailingAnchor).isActive = true
+        languageSwitch.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        languageSwitch.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+        languageSwitch.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+    }
+    
+    @objc func switchChangedState(langSwitch: UISwitch) {
+        viewModel.languageChangeAction(langSwitch.isOn ? Language.martian : Language.english)
+        viewModel.language = langSwitch.isOn ? Language.martian : Language.english
+        languageLabel.text = viewModel.language.rawValue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
