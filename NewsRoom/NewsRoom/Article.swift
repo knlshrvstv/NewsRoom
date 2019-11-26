8b07dfa32e499e7a66b20d163d790ef3af24ddfe
Kunal Shrivastava <knlshrvstv@gmail.com>
2019-11-25 22:54:33 -0800

    [Model] Make Article Equatable

--- NewsRoom/NewsRoom/Article.swift
+++ NewsRoom/NewsRoom/Article.swift
@@ -8,15 +8,28 @@
 
 import Foundation
 
-struct Article: Codable {
+struct Article: Codable, Equatable {
     let title: String
     let images: [Image]
     let body: String?
     
-    struct Image: Codable {
+    struct Image: Codable, Equatable {
         let topImage: Bool
         let url: String?
         let width: Int
         let height: Int
+        
+        static func ==(lhs: Image, rhs: Image) -> Bool {
+            return lhs.topImage == rhs.topImage
+                && lhs.url == rhs.url
+                && lhs.width == rhs.width
+                && lhs.height == rhs.height
+        }
+    }
+    
+    static func ==(lhs: Article, rhs: Article) -> Bool {
+        return lhs.title == rhs.title
+            && lhs.images == rhs.images
+            && lhs.body == rhs.body
     }
 }
